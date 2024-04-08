fec8d99920b3263e4d0abc879f3968dbb738a672
tshaax <tshaax@gmail.com>
2024-04-08 23:16:01 +0200

    added GetProduct

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndPoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+
+    public record GetProductResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.Map("/products", async (ISender sender) =>
+            {
+                var results = await sender.Send(new GetProductsQuery());
+
+                var response = results.Adapt<GetProductResponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetProducts")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
