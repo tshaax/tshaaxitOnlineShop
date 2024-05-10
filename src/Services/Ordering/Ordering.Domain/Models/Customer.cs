190a361224ed8991f0a1722e5758e5dfddef2424
tshaax <tshaax@gmail.com>
2024-05-10 04:23:06 +0200

    completed domain layer

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Models
+{
+    public class Customer : Entity<CustomerId>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        public static Customer Create (CustomerId id, string name, string email) 
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        private const int DefaultLegnth = 5;
+        public string Value { get; }
+
+        private OrderName(string value) => Value = value;
+
+        public static OrderName Of(string value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length , DefaultLegnth);
+
+            return new OrderName(value);
+        }
+    }
+}
